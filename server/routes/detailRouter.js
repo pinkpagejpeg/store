1fcd0b4478554e84ad823a0f29a68bc73b0b42aa
pinkpagejpeg <isip_a.a.stolbova@mpt.ru>
2024-02-10 15:02:10 +0300

    feat: add routes

---
+++ server/routes/detailRouter.js
@@ -0,0 +1,8 @@
+const Router = require('express')
+const router = new Router()
+
+router.post('/',) // Создание товара
+router.get('/',) // Получение всех товаров  
+router.get('/:id',) // Получение конкретного товара
+
+module.exports = router
---
+++ server/routes/userRouter.js
@@ -0,0 +1,8 @@
+const Router = require('express')
+const router = new Router()
+
+router.post('/registration',) // Регистрация
+router.post('/login',) // Авторизация
+router.get('/auth',) // Проверка на то, авторизован ли пользователь
+
+module.exports = router
