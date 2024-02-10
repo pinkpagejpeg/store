0c99bbc0dd6aa559db2ae1bed52680d1a4c661e9
pinkpagejpeg <isip_a.a.stolbova@mpt.ru>
2024-02-10 16:09:45 +0300

    feat: add controllers and error

---
+++ server/controllers/detailController.js
@@ -0,0 +1,15 @@
+class DetailController {
+    async create(req, res) {
+
+    }
+
+    async getAll(req, res) {
+
+    }
+
+    async getOne(req, res) {
+
+    }
+}
+
+module.exports = new DetailController()
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
+const userController = require('../controllers/userController')
 
-router.post('/registration',) // Регистрация
-router.post('/login',) // Авторизация
-router.get('/auth',) // Проверка на то, авторизован ли пользователь
+router.post('/registration', userController.registration) // Регистрация
+router.post('/login', userController.login) // Авторизация
+router.get('/auth', userController.check) // Проверка на то, авторизован ли пользователь
 
 module.exports = router
